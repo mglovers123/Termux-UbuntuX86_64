f48f5c6ec15e1384334a43ec3d539f96f51fa377
mglovers123 <71198980+mglovers123@users.noreply.github.com>
2020-12-14 21:06:16 +0700

    Update Ubuntu-AMD64.sh

--- Ubuntu-AMD64.sh
+++ Ubuntu-AMD64.sh
@@ -58,7 +58,7 @@ command+=" -b ubuntu-fs64/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
-#command+=" -b /sdcard"
+command+=" -b /sdcard"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
